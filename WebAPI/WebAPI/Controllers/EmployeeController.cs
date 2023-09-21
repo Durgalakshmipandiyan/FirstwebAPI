2c920c36f85bfe9c023223bbb818aeef92419958
durgalakshmi.p <durgalakshmi.p@200411LTP2741>
2023-09-21 18:44:20 +0530

    Employee find using nwind

--- WebAPI/WebAPI/Controllers/EmployeeController.cs
+++ WebAPI/WebAPI/Controllers/EmployeeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -7,5 +8,54 @@ namespace WebAPI.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        private RepositoryEmployee _repositoryEmployee;
+        public EmployeeController(RepositoryEmployee repository)
+        {
+            _repositoryEmployee = repository;
+        }
+        [HttpGet("/ListAllEmployees")]
+        public List<Employee> ListAllEmployees()
+        {
+            List<Employee> employeesList = _repositoryEmployee.AllEmployees();
+            return employeesList;
+        }
+        [HttpGet("/FindEmployee")]
+        public Employee FindEmployee(int id)
+        {
+            Employee employeeById = _repositoryEmployee.FindEmpoyeeById(id);
+            return employeeById;
+        }
+        [HttpPost("/AddEmployee")]
+        public string AddEmployee(Employee newEmployee)
+        {
+            int employeestatus = _repositoryEmployee.AddEmployee(newEmployee);
+            if (employeestatus == 0)
+            {
+                return "Employee Not Added To Database Since it already exist";
+            }
+            else
+            {
+                return "Employee Added To Database";
+            }
+        }
+        [HttpGet("/ModifyEmployee")]
+        public int ModifyEmployee(int id)
+        {
+            int employeestatus = _repositoryEmployee.ModifyEmployee(id);
+            return employeestatus;
+        }
+        [HttpGet("/DeleteEmployee")]
+        public string DeleteEmployee(int id)
+        {
+            int employeestatus = _repositoryEmployee.DeleteEmployee(id);
+            if (employeestatus == 0)
+            {
+                return "Employee does not exist in the Database";
+            }
+            else
+            {
+                return "Employee Successfully Deleted";
+            }
+        }
     }
 }
