54822a66fe29d1fa5214240fc7710ab6b3e5b6ed
Durgalakshmi  P <durgalakshmi.p@gavstech.com>
2023-09-21 12:07:39 +0530

    commitapi

---
+++ WebAPI/WebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        // api/Calculator/Add?x=45&y=98
+        [HttpGet("calulator/Add")] 
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        // api/Calculator/Add?x=95&y=9
+        [HttpPost("calculator/sum")]
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        // api/Calculator/Add?x=95&y=9
+        [HttpPut]
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpDelete]
+        // api/Calculator/Add?x=65&y=13 
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/Demo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Demo : ControllerBase
+    {
+        [HttpPost("demo/sum")]
+        public int Sum(int a, int b)
+        {
+            return a + b+500;
+        }
+    }
+}
